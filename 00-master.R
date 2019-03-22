6a2ff5f4aa58dee1af868c289ea96bc3cc7b1b41
Nathan Otto Siemers <nathan.siemers@bms.com>
2019-03-22 16:50:16 -0700

    added tmb creation (add_TMB.R) to 00-master.R

--- 00-master.R
+++ 00-master.R
@@ -3,13 +3,8 @@ files = c(
     'download.R',
     'create_db.R',
     'add_signatures_to_db.R'
+    'add_TMB.R'
     )
-
-## make simpler for now
-## library(rmarkdown)
-## lapply( files, function( thefile ) {
-##     render( thefile, pdf_document() )
-## })
 
 lapply( files, source, echo = TRUE, max.deparse.length = 20000 )
 

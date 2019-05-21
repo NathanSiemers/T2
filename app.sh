1ef483f5530d924db3779e57548f4f1f68f13644
Nathan Siemers <nathan@newair.local>
2019-05-21 04:34:13 -0700

    app.sh now only installs packages if missing

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 R -e 'if( !require("shinycssloaders") ) install.packages("shinycssloaders")'
-R -e 'if( !require("RMySql") ) install.packages("RMySQL")'
+R -e 'if( !require("RMySQL") ) install.packages("RMySQL")'
 R -e 'shiny::runApp("./", port=8888, host="0.0.0.0")'

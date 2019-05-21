e498f751a3b44c6d8e405f0edfd2ca72233204f3
Nathan Siemers <nathan@newair.local>
2019-05-21 04:29:01 -0700

    added require to condiditionally install needed packages in app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
-R -e 'install.packages("shinycssloaders")'
-R -e 'install.packages("RMySQL")'
+R -e 'if( !require("shinycssloaders") install.packages("shinycssloaders")'
+R -e 'if( !require("RMySql") install.packages("RMySQL")'
 R -e 'shiny::runApp("./", port=8888, host="0.0.0.0")'

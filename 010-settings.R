7dbd2e8e24c4318ef6c63312c6cde596d18f77be
Nathan Otto Siemers <nathan.siemers@bms.com>
2020-03-23 18:36:03 -0700

    force download

--- 010-settings.R
+++ 010-settings.R
@@ -2,8 +2,9 @@ TESTING = FALSE
 TESTINGLINES = 10
 mysql  = TRUE
 mysqldb = 'prod'    # dev or prod
-sqldestroy = FALSE
+sqldestroy = TRUE
 download = TRUE
 sqlvacuum = FALSE
 optimize = FALSE
 xena.force = TRUE
+
